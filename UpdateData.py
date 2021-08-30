f53764d3bcb3e9988e9aa3bef7d1671ba3c84415
Baris Karakulak <baris.1907.krklk@gmail.com>
2021-08-30 22:28:34 +0300

    added UpdateData

---
+++ UpdateData.py
@@ -0,0 +1,23 @@
+import pyrebase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+firebase_config={
+     "apiKey":os.getenv("apiKey") ,
+  "authDomain": os.getenv("authDomain"),
+  "projectId":os.getenv("projectId"),
+  "storageBucket":os.getenv("storageBucket") ,
+  "messagingSenderId": os.getenv("messagingSenderId"),
+  "appId":os.getenv("appId") ,
+  "measurementId":os.getenv("measurementId"),
+  "databaseURL":os.getenv("databaseURL")
+}
+
+
+firebase=pyrebase.initialize_app(firebase_config)
+db=firebase.database()
+data={"name":"Barış","surname":"Karakulak","age":21}
+
+
+db.child("Barış").update({"name":"Barış"})
