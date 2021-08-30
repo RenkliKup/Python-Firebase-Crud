944632387eebb6fb92a5891885ac07e70b8c82a0
Baris Karakulak <baris.1907.krklk@gmail.com>
2021-08-30 22:27:54 +0300

    added ListData

---
+++ ListData.py
@@ -0,0 +1,23 @@
+import pyrebase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+firebase_config={
+     "apiKey":os.getenv("apiKey") ,
+  "authDomain": os.getenv("authDomain"),
+  "projectId":os.getenv("projectId"),
+  "storageBucket":os.getenv("storageBucket") ,
+  "messagingSenderId": os.getenv("messagingSenderId"),
+  "appId":os.getenv("appId") ,
+  "measurementId":os.getenv("measurementId"),
+  "databaseURL":os.getenv("databaseURL")
+}
+
+
+firebase=pyrebase.initialize_app(firebase_config)
+db=firebase.database()
+
+
+for i in db.get().each():
+    print(i.val())
